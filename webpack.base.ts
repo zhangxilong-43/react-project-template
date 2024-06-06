96d780f5747df7f41e4c8f666940eea422aad8cc
zhangxilong01 <zhangxilong01@qianxin.com>
2024-06-07 00:00:38 +0800

    chore: init

---
+++ webpack.base.ts
@@ -0,0 +1,45 @@
+import { Configuration } from "webpack";
+import HtmlWebpackPlugin from "html-webpack-plugin";
+
+const path = require("path");
+
+const baseConfig: Configuration = {
+  entry: path.join(__dirname, "./src/index.tsx"), // 入口文件
+  // 打包出口文件
+  output: {
+    filename: "static/js/[name].js", // 每个输出js的名称
+    path: path.join(__dirname, "../dist"), // 打包结果输出路径
+    clean: true, // webpack4需要配置clean-webpack-plugin来删除dist文件,webpack5内置了
+    publicPath: "/", // 打包后文件的公共前缀路径
+  },
+  // loader 配置
+  module: {
+    rules: [
+      {
+        test: /.(ts|tsx)$/, // 匹配.ts, tsx文件
+        use: "babel-loader"
+      },
+      {
+        test: /.css$/, //匹配 css 文件
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".jsx", ".js"],
+  },
+  // plugins
+  plugins: [
+    new HtmlWebpackPlugin({
+      // 复制 'index.html' 文件，并自动引入打包输出的所有资源（js/css）
+      template: path.join(__dirname, "./public/index.html"),
+      // 压缩html资源
+      minify: {
+        collapseWhitespace: true, //去空格
+        removeComments: true, // 去注释
+      },
+    }),
+  ],
+};
+
+export default baseConfig
